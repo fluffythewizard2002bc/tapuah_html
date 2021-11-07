3b98b26cb02600e7f24029a24e9d1af42a1b03a0
fluffythewizard2002bc <69123883+fluffythewizard2002bc@users.noreply.github.com>
2021-11-07 13:11:20 +0200

    Add files via upload
    1.2

---
+++ woohoo2.js
@@ -0,0 +1,33 @@
+/*
+1. C
+2. A,B,D.
+3. A,B.
+4. A.
+5. C.
+6. A.
+7. B.
+*/
+
+
+let a=5;
+var b=5;
+const c=5;
+
+let d=55;
+d=20;
+
+
+let e=56;
+e='He\'s got it';
+console.log(e);
+
+let price=500;
+let tax=30;
+let total=price*tax/100 +price;
+console.log(total +'$');
+
+
+console.log(Math.round(50.6));
+
+let moo=undefined;
+console.log(typeof(moo));//6
