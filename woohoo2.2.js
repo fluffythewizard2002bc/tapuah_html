b88c4f4a52029e534be66c9c76b2a0a14f918bb6
fluffythewizard2002bc <69123883+fluffythewizard2002bc@users.noreply.github.com>
2021-11-07 14:23:53 +0200

    Add files via upload

---
+++ woohoo2.2.js
@@ -0,0 +1,6 @@
+function tellFortune(jobTitle,location,partnerName,numberOfChildren) {
+
+    return (console.log(`You will be a ${jobTitle} in ${location} and married to ${partnerName} with ${numberOfChildren} children.`));
+
+}
+tellFortune("programmer","Tel-Aviv","Sharon","2");
