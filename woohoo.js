f75ab30edd5c0340656d1266802cb50dabd15fee
fluffythewizard2002bc <69123883+fluffythewizard2002bc@users.noreply.github.com>
2021-11-07 12:35:09 +0200

    Add files via upload
    1.1

---
+++ woohoo.js
@@ -0,0 +1,19 @@
+
+
+let a='4';
+console.log(typeof(a));//1
+let b=4;
+console.log(typeof(b));//2
+const car = {type:"Fiat", model:"500", color:"white"};
+console.log(typeof(car));//3
+let booli=false;
+console.log(typeof(false));//4
+let foo=null;
+console.log(typeof(null));//5     typeof null  "object" (not "null" for legacy reasons)
+let moo=undefined;
+console.log(typeof(moo));//6
+let boo=Symbol('foo');
+console.log(typeof(boo));//7
+
+
+
