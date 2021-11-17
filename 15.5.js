7403a7a3156db8bc00897837863839cf7718aac3
fluffythewizard2002bc <69123883+fluffythewizard2002bc@users.noreply.github.com>
2021-11-18 01:27:11 +0200

    Add files via upload

---
+++ 15.5.js
@@ -0,0 +1,16 @@
+function countOccurrences(str, char) {
+    let counter = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str.charAt(i) === char) {
+            counter=counter + 1;
+        }
+    }
+    return counter;
+}
+console.log( countOccurrences("ini mini miny moe", "n"));
+
+
+/***
+ * 
+ * the counter isn't :counter=...
+ */
